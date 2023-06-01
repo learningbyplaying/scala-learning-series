6a0636b336ab58c405fac2e4eb8fcc7ab00fe53c
sergio <learningbyplaying>
2023-06-01 22:15:04 +0200

    ok

---
+++ src/main/scala/com/lbp/Basics.scala
@@ -0,0 +1,42 @@
+package com.lbp
+
+object Basics extends App {
+  val meaningOfLife: Int = 42
+
+  //meaningOfLife = 43
+  val aBoolean = false
+  val aString = "I love Scala!"
+
+  val aComposedString = "I" + "Other string"
+
+  val anInterpolatedString = s"The meaning $meaningOfLife"
+
+  // expressions
+  val anExpression = 2 + 3
+
+  val ifExpression = if(meaningOfLife > 43) 56 else 99
+
+  val chainedIfExpression = {
+    if (meaningOfLife > 41) 56
+    else if (meaningOfLife < 0) -2
+    else if (meaningOfLife > 98) 78
+    else 0
+  }
+  println(meaningOfLife,chainedIfExpression)
+
+  val aCodeBlock = {
+
+    val aLocalValue = 67
+    aLocalValue + 1
+  }
+
+  println(aCodeBlock)
+
+  def myFunction(x:Int, y:String): String = {
+    y + " " + x
+  }
+
+  println(myFunction(1,"test"))
+
+
+}
---
+++ src/main/scala/com/lbp/Playground.scala
@@ -0,0 +1,5 @@
+package com.lbp
+
+object Playground extends App {
+  println("I love Scala!")
+}
